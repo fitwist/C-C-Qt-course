98cdeff7151658c6bedb42b9d3757bc6f2596368
kapatsahelen@gmail.com <kapatsahelen@gmail.com>
2020-02-17 17:11:24 +0300

    Page memory editor attempt

--- 11_менеджер_страничной_памяти/page_memory_manager.cpp
+++ 11_менеджер_страничной_памяти/page_memory_manager.cpp
@@ -1,61 +1,46 @@
 #include <iostream>
-#include <iomanip>
-#include <vector>
+int max_size = 10000;
+int *heap = new int[max_size];
+int choice, page_size, page_size_2;
 
-#define TOTAL_MEMORY 10000
-#define MIN_PAGE_SIZE 1
-
-struct page
-{
-    int id;
-    int size;
-};
-std::vector<int> heap;
-
-void generate_pages()
-{ // автогенерация страниц
-    int total_memory = 0;
-    while (total_memory <= 10000) {
-        int randomId = rand() % 999 + 1;
-        int randomSize = rand() % 1999 + 1;
-        page list_elt {}
-    }
-}
-
-void remove_page() // удаление страницы
-{
-    std::cout << "Удаление страниц" << std::endl;
-}
-
-void merge_pages() // объединение страниц
-{
-    std::cout << "Объединение страниц" << std::endl;
-}
-
-int main()
-{
-    string separator = "   |   ";
-    int choice;
-    std::cout << "Добро пожаловать в менеджер памяти. Текущие страницы:" << std::endl;
-    std::cout << "ID  " << separator << "Размер" << std::endl;
-    std::cout << "——————————————————" << std::endl;
-    std::cout << "1058" << separator << "2245" << std::endl;
-    std::cout << "1001" << separator << "1024" << std::endl;
-    std::cout << "2359" << separator << "1400" << std::endl << std::endl;
+void merge_pages() {
     
-    std::cout << "1: Удаление задачи" << std::endl
-              << "2: Объединение задач" << std::endl;
-    std::cout << "Ваш выбор: ";
-    std::cin >> choice;
-    switch (choice) {
-        case 1:
-            remove_page();
-            break;
-        case 2:
-            merge_pages();
-            break;
-        default:
-            std::cout << "Выберите действительный пункт меню: " << std::endl;
+}
+
+void remove_page() {
+
+}
+
+int main() {
+    int total_memory = 0;
+    int i = 0;
+	std::cout << "Страницы автосгенерированы:" << std::endl;
+    while(total_memory <= max_size) {
+		*(heap+i) = rand()% 2000+1;
+        total_memory += *(heap+i);
+        std::cout << &heap[i] << ": " << *(heap + i) << std::endl; 
+        i++;
+	}
+
+    // std::cout << "\nВведите:" << std::endl;
+    // std::cout << "1, если хотите удалить страницу" << std::endl;
+    // std::cout << "2, если хотите объединить страницы" << std::endl;
+    // std::cout << "3, чтобы выйти. \nВыберите действие: ";
+    // scanf("%d", choice);
+    while (choice != 3) {
+        std::cin >> choice;
+        switch (choice) {
+            case 1:
+                std::cout << "Удаление страницы" << std::endl;
+                break;
+            case 2:
+                std::cout << "Объединение страниц" << std::endl;
+                break;
+            default:
+                std::cout << "Выберите действительный пункт меню:" << std::endl; 
+        }
     }
-    return 0;
+
+    delete heap;
+	return 0;
 }
